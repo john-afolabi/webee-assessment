0072fb48e4ca9a784108fa27ca6b7b488ffd18e5
John Afolabi <john.afolabi@payhippo.ng>
2023-04-30 07:22:00 +0100

    return events with workshops

--- express_sequelize/src/Modules/events/events.service.ts
+++ express_sequelize/src/Modules/events/events.service.ts
@@ -1,5 +1,5 @@
 import Event from './entities/event.entity';
-
+import Workshop from "./entities/workshop.entity"
 
 export class EventsService {
 
@@ -7,85 +7,20 @@ export class EventsService {
     return await Event.findAll();
   }
 
-  /* TODO: complete getEventsWithWorkshops so that it returns all events including the workshops
-    Requirements:
-    - maximum 2 sql queries
-    - verify your solution with `npm run test`
-    - do a `git commit && git push` after you are done or when the time limit is over
-    - Don't post process query result in javascript
-    Hints:
-    - open the `src/events/events.service` file
-    - partial or not working answers also get graded so make sure you commit what you have
-    Sample response on GET /events/events:
-    ```json
-    [
-      {
-        id: 1,
-        name: 'Laravel convention 2021',
-        createdAt: '2021-04-25T09:32:27.000000Z',
-        workshops: [
-          {
-            id: 1,
-            start: '2021-02-21 10:00:00',
-            end: '2021-02-21 16:00:00',
-            eventId: 1,
-            name: 'Illuminate your knowledge of the laravel code base',
-            createdAt: '2021-04-25T09:32:27.000000Z',
-          },
-        ],
-      },
-      {
-        id: 2,
-        name: 'Laravel convention 2023',
-        createdAt: '2023-04-25T09:32:27.000000Z',
-        workshops: [
-          {
-            id: 2,
-            start: '2023-10-21 10:00:00',
-            end: '2023-10-21 18:00:00',
-            eventId: 2,
-            name: 'The new Eloquent - load more with less',
-            createdAt: '2021-04-25T09:32:27.000000Z',
-          },
-          {
-            id: 3,
-            start: '2023-11-21 09:00:00',
-            end: '2023-11-21 17:00:00',
-            eventId: 2,
-            name: 'AutoEx - handles exceptions 100% automatic',
-            createdAt: '2021-04-25T09:32:27.000000Z',
-          },
-        ],
-      },
-      {
-        id: 3,
-        name: 'React convention 2023',
-        createdAt: '2023-04-25T09:32:27.000000Z',
-        workshops: [
-          {
-            id: 4,
-            start: '2023-08-21 10:00:00',
-            end: '2023-08-21 18:00:00',
-            eventId: 3,
-            name: '#NoClass pure functional programming',
-            createdAt: '2021-04-25T09:32:27.000000Z',
-          },
-          {
-            id: 5,
-            start: '2023-08-21 09:00:00',
-            end: '2023-08-21 17:00:00',
-            eventId: 3,
-            name: 'Navigating the function jungle',
-            createdAt: '2021-04-25T09:32:27.000000Z',
-          },
-        ],
-      },
-    ]
-    ```
-     */
 
   async getEventsWithWorkshops() {
-    throw new Error('TODO task 1');
+    const events = await Event.findAll({raw: true})
+    const workshops = await Workshop.findAll({raw: true})
+ 
+    const eventsWithWorkshops = events.map(event => {
+      const eventWorkshops = workshops.filter(workshop => workshop.eventId === event.id)  
+      return {
+        ...event,
+        workshops: eventWorkshops
+      }
+    })
+  
+    return eventsWithWorkshops
   }
 
   /* TODO: complete getFutureEventWithWorkshops so that it returns events with workshops, that have not yet started
@@ -155,6 +90,6 @@ export class EventsService {
     ```
      */
   async getFutureEventWithWorkshops() {
-    throw new Error('TODO task 2');
+  // return await Event.findAll({})
   }
 }
