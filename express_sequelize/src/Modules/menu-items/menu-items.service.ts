db0e03c68703270e87b56f33a15e12fd85b31825
John Afolabi <john.afolabi@payhippo.ng>
2023-04-30 08:50:36 +0100

    return menu items

--- express_sequelize/src/Modules/menu-items/menu-items.service.ts
+++ express_sequelize/src/Modules/menu-items/menu-items.service.ts
@@ -1,81 +1,33 @@
+import MenuItem from "./entities/menu-item.entity";
+
+interface IMenuItem {
+  id: number;
+  name: string;
+  url: string;
+  parentId: number | null;
+  createdAt: Date;
+  children?: IMenuItem[];
+}
 export class MenuItemsService {
-
-  /* TODO: complete getMenuItems so that it returns a nested menu structure
-    Requirements:
-    - your code should result in EXACTLY one SQL query no matter the nesting level or the amount of menu items.
-    - it should work for infinite level of depth (children of childrens children of childrens children, ...)
-    - verify your solution with `npm run test`
-    - do a `git commit && git push` after you are done or when the time limit is over
-    - post process your results in javascript
-    Hints:
-    - open the `src/menu-items/menu-items.service.ts` file
-    - partial or not working answers also get graded so make sure you commit what you have
-    Sample response on GET /menu:
-    ```json
-    [
-        {
-            "id": 1,
-            "name": "All events",
-            "url": "/events",
-            "parentId": null,
-            "createdAt": "2021-04-27T15:35:15.000000Z",
-            "children": [
-                {
-                    "id": 2,
-                    "name": "Laracon",
-                    "url": "/events/laracon",
-                    "parentId": 1,
-                    "createdAt": "2021-04-27T15:35:15.000000Z",
-                    "children": [
-                        {
-                            "id": 3,
-                            "name": "Illuminate your knowledge of the laravel code base",
-                            "url": "/events/laracon/workshops/illuminate",
-                            "parentId": 2,
-                            "createdAt": "2021-04-27T15:35:15.000000Z",
-                            "children": []
-                        },
-                        {
-                            "id": 4,
-                            "name": "The new Eloquent - load more with less",
-                            "url": "/events/laracon/workshops/eloquent",
-                            "parentId": 2,
-                            "createdAt": "2021-04-27T15:35:15.000000Z",
-                            "children": []
-                        }
-                    ]
-                },
-                {
-                    "id": 5,
-                    "name": "Reactcon",
-                    "url": "/events/reactcon",
-                    "parentId": 1,
-                    "createdAt": "2021-04-27T15:35:15.000000Z",
-                    "children": [
-                        {
-                            "id": 6,
-                            "name": "#NoClass pure functional programming",
-                            "url": "/events/reactcon/workshops/noclass",
-                            "parentId": 5,
-                            "createdAt": "2021-04-27T15:35:15.000000Z",
-                            "children": []
-                        },
-                        {
-                            "id": 7,
-                            "name": "Navigating the function jungle",
-                            "url": "/events/reactcon/workshops/jungle",
-                            "parentId": 5,
-                            "createdAt": "2021-04-27T15:35:15.000000Z",
-                            "children": []
-                        }
-                    ]
-                }
-            ]
-        }
-    ]
-  */
-
   async getMenuItems() {
-    throw new Error('TODO in task 3');
+    const menus = await MenuItem.findAll({ raw: true });
+
+    const buildMenu = (
+      items: MenuItem[],
+      parentId: number | null
+    ): IMenuItem[] => {
+      const menu = items.filter((item) => item.parentId === parentId);
+      return menu.map((item) => {
+        const children = buildMenu(items, item.id);
+        if (children.length) {
+          return { ...item, children };
+        }
+        return item;
+      });
+    };
+
+    const menuItems = buildMenu(menus, null);
+
+    return menuItems;
   }
 }
